3c8b2d3fd5490d2018f91c879ed56e28b52ba67e
vov <vov667@gmail.com>
2025-10-31 16:54:11 +0300

    наделал 4 примера спеллов

---
+++ spells/fFireball.gd
@@ -0,0 +1,26 @@
+extends Spell
+
+class_name FireballSpell
+
+
+func _init():
+	delay = 1
+	period = 0
+	casts = 1
+	#picture = 
+
+	spell_name = "Fireball"
+	damage = 5
+	heal = 0
+	type = 0
+	description = "Throw a basic fireball next round"
+
+
+func cast(pl: Player, en: Enemy, rythm: WorldRythm) -> void:
+	super.cast(pl, en, rythm)
+	
+
+# Метод для применения эффектов
+func apply() -> void:
+	enemy.take_damage(damage, type)
+	
