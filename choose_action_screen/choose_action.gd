b0bbff109b8353a2f8dabd866c2046ff5e098850
Sergey <aeaes1215@gmail.com>
2025-11-01 07:51:43 +0300

    new fixxx

--- choose_action_screen/choose_action.gd
+++ choose_action_screen/choose_action.gd
@@ -26,7 +26,9 @@ func block_pressed():
 	emit_signal("block")
 	
 func spell_book_pressed():
-	self.hide()
+	$Control.hide()
+	$mage.hide()
+	$new_enemy.hide()
 	book.show()
 	
 func action_pressed(action_name: String, spell_name: String = ""):
@@ -46,7 +48,9 @@ func spell_clicked(spell_name:String):
 		emit_signal("cast_spell", "spell", spell_name)
 		
 func return_to_actions():
-	self.show()
+	$Control.show()
+	$mage.show()
+	$new_enemy.show()
 	book.hide()
 	
 func change_visibity(flag: bool):
