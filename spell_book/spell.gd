77af1a0e034c5625ab27562c33a5f8e53cff212d
Sergey <aeaes1215@gmail.com>
2025-11-01 12:26:31 +0300

    Update spell.gd

--- spell_book/spell.gd
+++ spell_book/spell.gd
@@ -2,18 +2,17 @@ extends Control
 class_name SBookSpell
 
 signal clicked(spell_number: String)
-static var names = ["fireball", "fireball", "", "", ""]
-var assigned_spell: String
+static var names = ["", "", "", "", ""]
+var assigned_spell: String = "exit"
 @export var index: int = 0
 
 func _ready() -> void:
 	$Button.connect("button_down", spell_clicked)
-	assigned_spell = names[index]
-	
 
 func spell_clicked():
 	if self.name == "exit":
 		emit_signal("clicked", "exit")
 	else:
+		assigned_spell = names[index]
 		emit_signal("clicked", assigned_spell)
 	
