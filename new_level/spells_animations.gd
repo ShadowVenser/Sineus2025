3fe56412ec8b94e17a518c5488431902c8f0f304
Anime4nikVane4ka <pisarevim2015@gmail.com>
2025-11-01 15:26:12 +0300

    fixed_enemy_death

--- new_level/spells_animations.gd
+++ new_level/spells_animations.gd
@@ -4,5 +4,5 @@ const spells: Dictionary = {
 	"Fireball": [Vector2(954.0,334.0), Vector2(15.0,15.0), false], 
 	"Freeze": [Vector2(954.0,334.0), Vector2(15.0,15.0), false], 
 	"Scorched earth": [Vector2(954.0,404.0), Vector2(20.0,20.0), false], 
-	"Heal": [Vector2(954.0,304.0), Vector2(10.0,10.0), false],
+	"Heal": [Vector2(200.0,434.0), Vector2(12.0,12.0), false],
 	"Fire nova":[Vector2(554.0,304.0), Vector2(30.0,30.0), true]}
