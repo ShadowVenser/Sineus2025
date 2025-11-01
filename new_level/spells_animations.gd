0f3c060d1635b7cb134acb41a7de7b6b65a8bf10
Anime4nikVane4ka <pisarevim2015@gmail.com>
2025-11-01 17:25:29 +0300

    dementia_spells_cast

--- new_level/spells_animations.gd
+++ new_level/spells_animations.gd
@@ -8,4 +8,5 @@ const spells: Dictionary = {
 	"Self burn": [Vector2(200.0,434.0), Vector2(15.0,15.0), false],
 	"Self freeze": [Vector2(200.0,434.0), Vector2(15.0,15.0), false],
 	"Fire nova":[Vector2(554.0,304.0), Vector2(30.0,30.0), true],
+	"Time hold":[Vector2(300.0,304.0), Vector2(15.0,15.0), false],
 	"Death nova":[Vector2(200.0,400.0), Vector2(30.0,30.0), false]}
